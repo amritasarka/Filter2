863610f19bc496d616d3480b5a1b4d0dea199dc0
Amrita <amritasar254@gmail.com>
2023-03-05 16:37:48 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Table from './Table';
+
+ReactDOM.render(
+    <Table/>,
+
+ document.getElementById('root'));
+
+
